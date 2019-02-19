9d26324cf84c38791f2f5ba0fc78662de133571f
Chat Sumlin <jcsumlin@gmail.com>
2019-02-18 19:20:35 -0600

    Moved JS

--- js/semantic-ui.js
+++ js/semantic-ui.js
@@ -19,11 +19,6 @@
  *
  */
 
- /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
- particlesJS.load('particles-js', 'particles.js-master/particles.json', function() {
-   console.log('callback - particles.js config loaded');
- });
-
 ;(function ( $, window, document, undefined ) {
 
 $.site = $.fn.site = function(parameters) {
